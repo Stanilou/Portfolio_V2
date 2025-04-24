5ae54347d32c13fba01d932069f0e90d6531552c
StanislasROLLAND <stanislasrolland05@gmail.com>
2025-04-24 13:54:40 +0200

    Update Portfolio

---
+++ Scripts/main.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const header = document.querySelector("header");
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 50) {
+            header.classList.add("scrolled");
+        } else {
+            header.classList.remove("scrolled");
+        }
+    });
+
+    const links = document.querySelectorAll("header > div > ul > li > a");
+    const updateActiveLink = () => {
+        let currentSection = null;
+        document.querySelectorAll("main > div").forEach(section => {
+            const rect = section.getBoundingClientRect();
+            if (rect.top <= 0 && rect.bottom >= 0) {
+                currentSection = section.id;
+            }
+        });
+        links.forEach(link => {
+            if (link.getAttribute("href").substring(1) === currentSection) {
+                link.classList.add("active");
+            } else {
+                link.classList.remove("active");
+            }
+        });
+    };
+    window.addEventListener("scroll", updateActiveLink);
+    updateActiveLink();
+    links.forEach(link => {
+        link.addEventListener("click", (event) => {
+            links.forEach(link => link.classList.remove("active"));
+            event.target.classList.add("active");
+        });
+    });
+});
